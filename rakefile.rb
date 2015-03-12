1b4d078309df65ee36242f8b55ce64d1bd45d9ba
Yoann Jaspar <yoannj@acenda.com>
2015-03-12 13:29:12 -0700

    Removed config json and moved rakefile to root directory

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,7 @@
 begin
     require 'rspec/core/rake_task'
     RSpec::Core::RakeTask.new(:spec) do |t|
-        t.pattern = 'specs/*_spec.rb'
+        t.pattern = 'tests/specs/*_spec.rb'
         t.rspec_opts = "-c --format documentation"
         t.verbose = true
     end
